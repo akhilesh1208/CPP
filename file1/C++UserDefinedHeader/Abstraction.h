5793fd176ffe2cb60a2c035f50ebe1973f6d9de7
Akhilesh Kumar Shastri <76882362+akhilesh1208@users.noreply.github.com>
2024-04-19 18:17:56 +0530

    Add files via upload

---
+++ file1/C++UserDefinedHeader/Abstraction.h
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ class XYZ{
+     int data;
+ public:
+     XYZ();
+     XYZ(int);
+     ~XYZ();
+//      void fun();
+    
+     void fun(int);
+ //  void funOne();
+ };
---
+++ file1/C++UserDefinedHeader/Main.cpp
@@ -0,0 +1,12 @@
+#include"Abstraction.h"
+
+int main(){
+    XYZ a,b=10;
+//     a.fun();
+    a.fun(8);
+    
+//     b.fun();
+    b.fun(9);
+}
+
+    
